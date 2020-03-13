6ec022eb1d32e02def719d76c39edd9753bf00b4
Khanh <tran_khanh@outlook.com>
2020-03-13 08:19:12 +0700

    bai giai

---
+++ content/csharp/Exercise/tinhcvdthinhtron.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace VietJackCsharp
+{
+    class TestCsharp
+    {
+        public static void Main()
+        {
+
+            double r, chu_vi, dien_tich;
+            double PI = 3.14;
+            Console.Write("Nhap ban kinh duong tron: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            chu_vi = 2 * PI * r;
+            Console.WriteLine("\nChu vi duong tron la: {0}", chu_vi);
+            dien_tich = PI * r * r;
+            Console.WriteLine("Dien tich duong tron la: {0}", dien_tich);
+            Console.Read();  
+
+            Console.ReadKey();
+        } 
+    }
+}
